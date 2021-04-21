e9f2edaa07b356b8b1be9ecfb0736155b0795838
vigorjs <80097275+vigorjs@users.noreply.github.com>
2021-04-21 19:33:18 +0700

    minggu ke 7
    minggu 7 crud

---
+++ minggu-7/crud/home.php
@@ -0,0 +1,28 @@
+<h1>welcome to pdo crud!</h1>
+<h2>Tambah data sekolah</h2>
+<form method="post" action="proses/sekolah.save.php" enctype="multipart/form-data">
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td><input type="text" name="nama" value=""></td>
+
+        </tr>
+
+        <tr>
+            <td>Alamat</td>
+            <td><input type="text" name="alamat" value=""></td>
+        </tr>
+
+        <tr>
+            <td>Logo</td>
+            <td><input type="text" name="logo" value=""></td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td><button type="submit" name="">Simpan</button></td>
+        </tr>
+
+    </table>
+   
+</form>
